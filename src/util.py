dfc5e0abc1411c9ce24a75803d163961804bcbb3
samwang0722 <74766713+samwang0722@users.noreply.github.com>
2023-03-03 16:56:07 +0800

    update combine pdf

--- src/util.py
+++ src/util.py
@@ -2,15 +2,51 @@
 # -*-coding:UTF-8-*-
 
 import os
-from PyPDF3 import PdfFileReader, PdfFileMerger
+from pdf2image import convert_from_path
+from PyPDF2 import PdfFileReader, PdfFileMerger, PdfFileWriter
+
+
+def read_encrypt_pdf(file_name):
+    pages = convert_from_path(file_name, dpi=300)
+    pages[0].resize((842, 595))
+    pages[0].save('./temp/temp.pdf')
+
+    pdf_reader = PdfFileReader(open('./temp/temp.pdf', 'rb'))
+    # 設置頁面大小為 A4
+    # 創建 PDF 寫入器
+    pdf_writer = PdfFileWriter()
+
+    # 遍歷每個頁面
+    for i in range(pdf_reader.getNumPages()):
+        page = pdf_reader.getPage(i)
+        # 計算新的頁面大小
+        new_page_width = 842
+        new_page_height = 595
+
+        # 創建新的頁面對象
+        new_page = pdf_writer.addBlankPage(new_page_width,
+                                           new_page_height)
+
+        # 縮放頁面並寫入到新的頁面對象中
+        new_page.mergeScaledTranslatedPage(page, 0.24, 0,
+                                           0)
+
+    # 將調整後的PDF寫入新的檔案
+    with open('./temp/temp2.pdf', 'wb') as out:
+        pdf_writer.write(out)
+
+    file = PdfFileReader(open('./temp/temp2.pdf', 'rb'))
+    return file
 
 
 def combine_pdf(files, out_path):
     merger = PdfFileMerger(strict=False)
     for f in files:
         file = PdfFileReader(open(f, 'rb'))
+
         if file.isEncrypted:
-            file.decrypt('Aa3662090')  # 密碼請自行更改
+            file = read_encrypt_pdf(f)
+
         merger.append(file)
     merger.write(out_path)
     merger.close()
@@ -32,14 +68,7 @@ if len(target_file) == 0:
 elif len(target_file) == 1:
     combine_pdf(files=[file_name,
                        target_folder+target_file[0]],
-                out_path=out_path)
+                out_path=out_path+file_code+'.pdf')
     msg = 'OK'
 else:
     msg = 'Matching multiple files'
-
-
-
-
-from pdf2image import convert_from_path
-pages = convert_from_path('C:/Users/wang/Desktop/11112_14660853_營業人銷售額與稅額申報書(401)_.pdf', dpi=200, first_page=1, last_page=1)
-pages[0].save('example.jpg', 'JPEG')
